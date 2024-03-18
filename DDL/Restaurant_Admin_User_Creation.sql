0d54890f423d29724ecbde790849a2db6681cef3
Sai Geeta Acharya <144739067+Sai-Geeta-Acharya@users.noreply.github.com>
2024-03-18 18:45:34 -0400

    Updated Restaurant_Admin_User_Creation.sql with proper comments and used all_users instead

--- DDL/Restaurant_Admin_User_Creation.sql
+++ DDL/Restaurant_Admin_User_Creation.sql
@@ -1,13 +1,5 @@
-/**
-
-create user restaurant_admin identified by DineEase2024;
-grant connect to restaurant_admin;
-GRANT CREATE SESSION, CREATE TABLE, CREATE SEQUENCE, CREATE PROCEDURE, CREATE TRIGGER, CREATE USER TO restaurant_admin WITH ADMIN OPTION;
-GRANT CONNECT TO restaurant_admin WITH ADMIN OPTION;
-GRANT CREATE ROLE TO restaurant_admin WITH ADMIN OPTION;
-GRANT UNLIMITED TABLESPACE TO restaurant_admin;
-GRANT CREATE VIEW TO restaurant_admin;
-
+/** 
+''' This is done by ADMIN USER to create a resturant_admin for this project'''
 **/
 
 set serveroutput on;
@@ -15,19 +7,23 @@ Declare
     user_exists NUMBER;
     grant_error EXCEPTION;
 BEGIN
+    -- Check if the user already exists
     SELECT COUNT(*)
     INTO user_exists
-    FROM dba_users
+    FROM all_users
     WHERE username = 'RESTAURANT_ADMIN';
 
     IF user_exists = 0 THEN
+        -- Create the user if it doesn't exist
         EXECUTE IMMEDIATE 'CREATE USER restaurant_admin IDENTIFIED BY DineEase2024';
         DBMS_OUTPUT.PUT_LINE('User created successfully.');
     ELSE
+        -- User already exists, inform the user
         DBMS_OUTPUT.PUT_LINE('User already exists.');
     END IF;
 
     BEGIN
+        -- User already exists, inform the user
         EXECUTE IMMEDIATE 'GRANT CONNECT TO restaurant_admin';
         EXECUTE IMMEDIATE 'GRANT CREATE SESSION, CREATE TABLE, CREATE SEQUENCE, CREATE PROCEDURE, CREATE TRIGGER, CREATE USER TO restaurant_admin WITH ADMIN OPTION';
         EXECUTE IMMEDIATE 'GRANT CONNECT TO restaurant_admin WITH ADMIN OPTION';
@@ -36,7 +32,7 @@ BEGIN
         EXECUTE IMMEDIATE 'GRANT CREATE VIEW TO restaurant_admin';
     EXCEPTION
         WHEN grant_error THEN
-            NULL; 
+            NULL; -- Ignore if the user already has the privilege
     END;
 EXCEPTION
     WHEN OTHERS THEN

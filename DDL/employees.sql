59b76e79b261602e694fdd0dc966be1860c9317b
Sai Geeta Acharya <144739067+Sai-Geeta-Acharya@users.noreply.github.com>
2024-03-19 18:01:41 -0400

    v_null_constraint_violation on employees table

--- DDL/employees.sql
+++ DDL/employees.sql
@@ -1,47 +1,118 @@
-set SERVEROUTPUT on;
-
+set serveroutput on;
 DECLARE
-    -- variable to store the count of existing employees table
-    table_exists NUMBER;
+    v_gender_too_long EXCEPTION;
+    PRAGMA EXCEPTION_INIT(v_gender_too_long, -12899); -- Value too large for column error code
+
+    v_constraint_violation_pk EXCEPTION;
+    PRAGMA EXCEPTION_INIT(v_constraint_violation_pk, -1); -- Primary key violation error code
+
+    v_constraint_violation_unique EXCEPTION;
+    PRAGMA EXCEPTION_INIT(v_constraint_violation_unique, -2299); -- Unique constraint violation error code
+    
+    v_null_constraint_violation EXCEPTION;
+    PRAGMA EXCEPTION_INIT(v_null_constraint_violation, -1400); -- Null constraint violation error code
+    
 BEGIN
-     -- Check if the employees table already exists
-    SELECT
-        COUNT(*)
-    INTO table_exists
-    FROM
-        user_tables
-    WHERE
-        table_name = 'EMPLOYEES';
+    -- Insert data into the Employees table
+    BEGIN
+        INSERT INTO EMPLOYEES (employee_id, fname, lname, street, city, state, zip, country, salary, gender, ssn, start_date, end_date, role)
+        VALUES (101, 'Geet', 'Acharya', '123 Washington St', 'Boston', 'Massachusetts', '01235', 'USA', 50000.00, 'F', '123-45-6789', TO_DATE('2024-01-01', 'YYYY-MM-DD'), NULL, 'Manager');
+    EXCEPTION
+        WHEN v_constraint_violation_pk THEN
+            DBMS_OUTPUT.PUT_LINE('Duplicate employee_id found. Skipping insertion.');
+        WHEN v_gender_too_long THEN
+            DBMS_OUTPUT.PUT_LINE('Value too large for Gender. Skipping insertion.');
+        WHEN v_constraint_violation_unique THEN
+            DBMS_OUTPUT.PUT_LINE('Duplicate ssn found. Skipping insertion.');
+        WHEN v_null_constraint_violation THEN
+            DBMS_OUTPUT.PUT_LINE('First name / Role cannot be null. Skipping insertion.');
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+    END;
+
+    BEGIN
+        INSERT INTO EMPLOYEES (employee_id, fname, lname, street, city, state, zip, country, salary, gender, ssn, start_date, end_date, role)
+        VALUES (102, 'Emily', 'Watt', '456 Elm St', 'Jersey City', 'New Jersey', '07302', 'USA', 45000.00, 'F', '987-65-4321', TO_DATE('2024-02-01', 'YYYY-MM-DD'), NULL, 'Cashier');
+    EXCEPTION
+        WHEN v_constraint_violation_pk THEN
+            DBMS_OUTPUT.PUT_LINE('Duplicate employee_id found. Skipping insertion.');
+        WHEN v_gender_too_long THEN
+            DBMS_OUTPUT.PUT_LINE('Value too large for Gender. Skipping insertion.');
+        WHEN v_constraint_violation_unique THEN
+            DBMS_OUTPUT.PUT_LINE('Duplicate ssn found. Skipping insertion.');
+        WHEN v_null_constraint_violation THEN
+            DBMS_OUTPUT.PUT_LINE('First name / Role cannot be null. Skipping insertion.');
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+    END;
     
-     -- If employees table does not exist, create it        
-    IF table_exists = 0 THEN
-        EXECUTE IMMEDIATE '
-        CREATE TABLE EMPLOYEES (
-            employee_id INTEGER NOT NULL,
-            fname       VARCHAR2(25),
-            lname       VARCHAR2(25),
-            street      VARCHAR2(255),
-            city        VARCHAR2(100),
-            state       VARCHAR2(100),
-            zip         VARCHAR2(10),
-            country     VARCHAR2(100),
-            salary      NUMBER(10, 2),
-            gender      CHAR(1),
-            ssn         VARCHAR2(11),
-            start_date  DATE,
-            end_date    DATE,
-            role  VARCHAR2(45),
-            CONSTRAINT employees_pk PRIMARY KEY (employee_id)
-        )';
-        dbms_output.put_line('EMPLOYEES table created successfully.');
-    ELSE
-         -- If EMPLOYEES table already exists, inform the user
-        dbms_output.put_line('EMPLOYEES table already exists.');
-    END IF;
+    BEGIN
+        INSERT INTO EMPLOYEES (employee_id, fname, lname, street, city, state, zip, country, salary, gender, ssn, start_date, end_date, role)
+        VALUES (103, 'Dough', 'Heffernen', '456 Brokline Ave', 'Hillsborough', 'New York', '06782', 'USA', 35000.00, 'M', '987-65-8321', TO_DATE('2024-01-15', 'YYYY-MM-DD'), NULL, 'Service_crew');
+    EXCEPTION
+        WHEN v_constraint_violation_pk THEN
+            DBMS_OUTPUT.PUT_LINE('Duplicate employee_id found. Skipping insertion.');
+        WHEN v_gender_too_long THEN
+            DBMS_OUTPUT.PUT_LINE('Value too large for Gender. Skipping insertion.');
+        WHEN v_constraint_violation_unique THEN
+            DBMS_OUTPUT.PUT_LINE('Duplicate ssn found. Skipping insertion.');
+        WHEN v_null_constraint_violation THEN
+            DBMS_OUTPUT.PUT_LINE('First name / Role cannot be null. Skipping insertion.');
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+    END;
     
--- Exception handling block to catch any errors during table creation
-EXCEPTION
-    WHEN OTHERS THEN
-        dbms_output.put_line('Error creating table EMPLOYEES: ' || sqlerrm);
+    BEGIN
+        INSERT INTO EMPLOYEES (employee_id, fname, lname, street, city, state, zip, country, salary, gender, ssn, start_date, end_date, role)
+        VALUES (104, 'Carry', 'Heffernen', '406 Amory St', 'Cambridge', 'Massachusetts', '07302', 'USA', 35000.00, 'F', '897-65-7321', TO_DATE('2024-02-05', 'YYYY-MM-DD'), NULL, 'Service_crew');
+    EXCEPTION
+        WHEN v_constraint_violation_pk THEN
+            DBMS_OUTPUT.PUT_LINE('Duplicate employee_id found. Skipping insertion.');
+        WHEN v_gender_too_long THEN
+            DBMS_OUTPUT.PUT_LINE('Value too large for Gender. Skipping insertion.');
+        WHEN v_constraint_violation_unique THEN
+            DBMS_OUTPUT.PUT_LINE('Duplicate ssn found. Skipping insertion.');
+        WHEN v_null_constraint_violation THEN
+            DBMS_OUTPUT.PUT_LINE('First name / Role cannot be null. Skipping insertion.');
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+    END;
+    
+    BEGIN
+        INSERT INTO EMPLOYEES (employee_id, fname, lname, street, city, state, zip, country, salary, gender, ssn, start_date, end_date, role)
+        VALUES (105, 'Ross', 'Geller', '456 Elm St', 'Lowell', 'Massachusetts', '07302', 'USA', 40000.00, 'M', '387-65-4121', TO_DATE('2024-01-01', 'YYYY-MM-DD'), NULL, 'Kitchen_crew');
+    EXCEPTION
+        WHEN v_constraint_violation_pk THEN
+            DBMS_OUTPUT.PUT_LINE('Duplicate employee_id found. Skipping insertion.');
+        WHEN v_gender_too_long THEN
+            DBMS_OUTPUT.PUT_LINE('Value too large for Gender. Skipping insertion.');
+        WHEN v_constraint_violation_unique THEN
+            DBMS_OUTPUT.PUT_LINE('Duplicate ssn found. Skipping insertion.');
+        WHEN v_null_constraint_violation THEN
+            DBMS_OUTPUT.PUT_LINE('First name / Role cannot be null. Skipping insertion.');
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+    END;
+    
+    BEGIN
+        INSERT INTO EMPLOYEES (employee_id, fname, lname, street, city, state, zip, country, salary, gender, ssn, start_date, end_date, role)
+        VALUES (120, 'John', 'Kenedy', '4156 Warren St', 'Watertown', 'Massachusetts', '07102', 'USA', 40000.00, 'M', '907-65-4171', TO_DATE('2024-01-01', 'YYYY-MM-DD'), NULL, 'Kitchen_crew');
+        DBMS_OUTPUT.PUT_LINE('Record Inserted.');
+    
+    EXCEPTION
+        WHEN v_constraint_violation_pk THEN
+            DBMS_OUTPUT.PUT_LINE('Duplicate employee_id found. Skipping insertion.');
+        WHEN v_gender_too_long THEN
+            DBMS_OUTPUT.PUT_LINE('Value too large for Gender. Skipping insertion.');
+        WHEN v_constraint_violation_unique THEN
+            DBMS_OUTPUT.PUT_LINE('Duplicate ssn found. Skipping insertion.');
+        WHEN v_null_constraint_violation THEN
+            DBMS_OUTPUT.PUT_LINE('First name / Role cannot be null. Skipping insertion.');
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE('An error occurred: ' || SQLERRM);
+    END;
+    
+    -- Commit the transaction
+    COMMIT;
 END;
 /

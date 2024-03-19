8b702b4ade0ad24346fc5946576ad6d6b57a3d18
Sai Geeta Acharya <144739067+Sai-Geeta-Acharya@users.noreply.github.com>
2024-03-19 16:59:59 -0400

    Update employees_insert.sql

--- DML/employees_insert.sql
+++ DML/employees_insert.sql
@@ -1,17 +1,17 @@
 INSERT INTO EMPLOYEES (employee_id, fname, lname, street, city, state, zip, country, salary, gender, ssn, start_date, end_date, role)
-VALUES (1, 'Geet', 'Acharya', '123 Washington St', 'Boston', 'Massachusetts', '01235', 'USA', 50000.00, 'F', '123-45-6789', TO_DATE('2024-01-01', 'YYYY-MM-DD'), NULL, 'Manager');
+VALUES (101, 'Geet', 'Acharya', '123 Washington St', 'Boston', 'Massachusetts', '01235', 'USA', 50000.00, 'F', '123-45-6789', TO_DATE('2024-01-01', 'YYYY-MM-DD'), NULL, 'Manager');
 
 INSERT INTO EMPLOYEES (employee_id, fname, lname, street, city, state, zip, country, salary, gender, ssn, start_date, end_date, role)
-VALUES (2, 'Emily', 'Watt', '456 Elm St', 'Jersey City', 'New Jersey', '07302', 'USA', 45000.00, 'F', '987-65-4321', TO_DATE('2024-02-01', 'YYYY-MM-DD'), NULL, 'Cashier');
+VALUES (102, 'Emily', 'Watt', '456 Elm St', 'Jersey City', 'New Jersey', '07302', 'USA', 45000.00, 'F', '987-65-4321', TO_DATE('2024-02-01', 'YYYY-MM-DD'), NULL, 'Cashier');
 
 INSERT INTO EMPLOYEES (employee_id, fname, lname, street, city, state, zip, country, salary, gender, ssn, start_date, end_date, role)
-VALUES (3, 'Dough', 'Heffernen', '456 Brokline Ave', 'Hillsborough', 'New York', '06782', 'USA', 35000.00, 'M', '987-65-8321', TO_DATE('2024-01-15', 'YYYY-MM-DD'), NULL, 'Service_crew');
+VALUES (103, 'Dough', 'Heffernen', '456 Brokline Ave', 'Hillsborough', 'New York', '06782', 'USA', 35000.00, 'M', '987-65-8321', TO_DATE('2024-01-15', 'YYYY-MM-DD'), NULL, 'Service_crew');
 
 INSERT INTO EMPLOYEES (employee_id, fname, lname, street, city, state, zip, country, salary, gender, ssn, start_date, end_date, role)
-VALUES (4, 'Carry', 'Heffernen', '406 Amory St', 'Cambridge', 'Massachusetts', '07302', 'USA', 35000.00, 'F', '897-65-7321', TO_DATE('2024-02-05', 'YYYY-MM-DD'), NULL, 'Service_crew');
+VALUES (104, 'Carry', 'Heffernen', '406 Amory St', 'Cambridge', 'Massachusetts', '07302', 'USA', 35000.00, 'F', '897-65-7321', TO_DATE('2024-02-05', 'YYYY-MM-DD'), NULL, 'Service_crew');
 
 INSERT INTO EMPLOYEES (employee_id, fname, lname, street, city, state, zip, country, salary, gender, ssn, start_date, end_date, role)
-VALUES (5, 'Ross', 'Geller', '456 Elm St', 'Lowell', 'Massachusetts', '07302', 'USA', 40000.00, 'M', '387-65-4121', TO_DATE('2024-01-01', 'YYYY-MM-DD'), NULL, 'Kitchen_crew');
+VALUES (105, 'Ross', 'Geller', '456 Elm St', 'Lowell', 'Massachusetts', '07302', 'USA', 40000.00, 'M', '387-65-4121', TO_DATE('2024-01-01', 'YYYY-MM-DD'), NULL, 'Kitchen_crew');
 
 Commit;
 
